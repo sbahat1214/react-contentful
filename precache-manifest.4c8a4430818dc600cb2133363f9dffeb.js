ebd345f0da5a8a07240c81c53599a8ea4e94ba27
sbahat1214 <bcs.f15.43@gmail.com>
2019-12-27 03:08:19 +0500

    React + Redux + Contentful + redux Thunk + React Router + Materialui

---
+++ precache-manifest.4c8a4430818dc600cb2133363f9dffeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37af7a6ef3f14b335c8fe6f3c3e1c761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d9869d107acff82beb7",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "79e8ae93fa8ac3d5be5f",
+    "url": "/static/css/main.55792f82.chunk.css"
+  },
+  {
+    "revision": "3d9869d107acff82beb7",
+    "url": "/static/js/2.99b23ab6.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/static/js/2.99b23ab6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79e8ae93fa8ac3d5be5f",
+    "url": "/static/js/main.1588b860.chunk.js"
+  },
+  {
+    "revision": "f4fd2fe2d2780f84f762",
+    "url": "/static/js/runtime-main.ea4cb42b.js"
+  },
+  {
+    "revision": "fd249939f9be10c4d10fb9ca7e92497d",
+    "url": "/static/media/bgImage.fd249939.jpg"
+  }
+]);
